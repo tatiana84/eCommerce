236ea231115227a682885a50d9226251d17a7a42
tatiana84 <tantamuntean2018@gmail.com>
2020-08-06 15:02:56 +0300

    built formButton component in formfields.js and added submit/login button to signinform

--- src/components/auth/signinForm.js
+++ src/components/auth/signinForm.js
@@ -2,13 +2,13 @@ import React, { Component } from "react";
 
 import { reduxForm, Field } from "redux-form";
 
-import { FormInput } from "../formFields";
+import { FormInput, FormButton } from "../formFields";
 
 class SignInForm extends Component {
     render() {
-        const { className } = this.props;
+        const { className, handleSubmit } = this.props;
         return (
-            <form className={`${className} sign-in-form`}>
+            <form onSubmit={handleSubmit} className={`${className} sign-in-form`}>
                 <Field className="sign-in-form__email" 
                     type="email" 
                     title="Email" 
@@ -22,6 +22,13 @@ class SignInForm extends Component {
                     placeholder="Password" 
                     name="password" 
                     component={FormInput}
+                />
+                <Field className="sign-in-form__login"
+                    onClick={() => console.log('try an submit')} 
+                    type="submit" 
+                    title="Login" 
+                    name="login" 
+                    component={FormButton}
                 />
             </form>
         )
