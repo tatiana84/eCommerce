51e77f0fe4375a105e6080008ca3212aacc7c739
tatiana84 <tantamuntean2018@gmail.com>
2020-08-12 20:33:06 +0300

    finished header feature

--- src/components/account/account.js
+++ src/components/account/account.js
@@ -10,6 +10,19 @@ class Account extends Component {
 
     componentDidMount() {
 
+        const headerLinks = [
+            {
+                _id: 0,
+                title: "Shop",
+                path: "/shop"
+            },
+            {
+                _id: 1,
+                title: "Logout",
+                path: "/"
+            }
+        ]
+
         const navbarLinks = [
             {
                 _id: 0,
