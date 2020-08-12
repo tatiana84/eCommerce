37a9753fc2a49bb37e2ebd718fe577bea51d2a6e
tatiana84 <tantamuntean2018@gmail.com>
2020-08-12 17:14:48 +0300

    built base structure for account components

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -12,6 +12,7 @@ import history from "./history";
 import Layout from "./components/layout";
 import Signin from "./components/auth/signin";
 import Signup from "./components/auth/signup";
+import Account from "./components/account/account";
 
 function main() {
   ReactDOM.render(
@@ -22,6 +23,8 @@ function main() {
             <Route path="/" exact component={Signin}/>
             <Route path="/signin" exact component={Signin}/>
             <Route path="/signup" exact component={Signup}/>
+
+            <Route path="/account" exact component={Account}/>
           </Switch>
         </Layout>
       </Router>
---
+++ src/components/account/accountInformation.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class AccountInformation extends Component {
+    render() {
+        return (
+            <div>
+                Account Information
+            </div>
+        )
+    }
+}
+
+export default AccountInformation;
